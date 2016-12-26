ca31d524fbb4fd95d261b029bcf30bd70dc92bc7
jeremyfrancis <francisj12@icloud.com>
2016-12-25 20:44:18 -0500

    Production and Development push certificates swapped for testing.

--- index.js
+++ index.js
@@ -25,12 +25,12 @@ var api = new ParseServer({
     {
       pfx: 'pollar_push_development', // Dev PFX or P12
       bundleId: 'com.Pollar',
-      production: false // Dev
+      production: true // Dev
     },
     {
       pfx: 'pollar_push_production', // Prod PFX or P12
       bundleId: 'com.Pollar',
-      production: true // Prod
+      production: false // Prod
     }
   ]
 }
