a8d09f4c6f418ecedff0ded871b9ad8fd6cde6b9
jeremyfrancis <francisj12@icloud.com>
2016-12-26 23:35:11 -0500

    Updated main

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://admin:pollarapp1@ds011745.mlab.com:11745/pollar',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
+  appId: process.env.APP_ID || 'kn8vaob9zMGYcYV8FOVLmS7IbdGLNLzCtUIafcPP',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
