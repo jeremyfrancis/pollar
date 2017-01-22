7463a066aa589375fbfbb1418479e7f9ad2578be
jeremyfrancis <francisj12@icloud.com>
2017-01-22 14:39:53 -0500

    added files adapter

--- index.js
+++ index.js
@@ -11,12 +11,16 @@ if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
+var FSFilesAdapter = require('parse-server-files-adapter');
+var fsAdapter = new FSFilesAdapter();
+
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   fileKey:'066c8b87-1419-4678-aeb1-0605657cecdb',
+  filesAdapter: fsAdapter,
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
